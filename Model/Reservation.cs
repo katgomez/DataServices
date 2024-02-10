08b15e68091829a053b12a087d2c132f21d808b5
Katherine Gomez <kat.gomez.z@gmail.com>
2024-02-10 22:00:11 +0100

    adding more models

---
+++ Model/Reservation.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WS.DataServices.Model
+{
+    public class Reservation
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public DateOnly ReservationDate { get; set; }
+        public int NumberOfClients { get; set; }
+        public string ReservationStatus { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
