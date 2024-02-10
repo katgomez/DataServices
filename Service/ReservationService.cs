b8515979e831250cff35447c2c3079c9fbc9d12a
Katherine Gomez <kat.gomez.z@gmail.com>
2024-02-10 22:22:11 +0100

    added implementations for reservation

--- Service/ReservationService.cs
+++ Service/ReservationService.cs
@@ -1,4 +1,6 @@
-﻿using WS.DataServices.Model;
+﻿using System.ServiceModel;
+using WS.DataServices.Model;
+using WS.Unit06.Example2.Data;
 
 namespace WS.DataServices.Service;
 
@@ -6,21 +8,40 @@ public class ReservationService: IReservationServices
 {
     public Reservation[] GetReservations()
     {
-        throw new NotImplementedException();
+        using (DAOFactory factory = new DAOFactory())
+        {
+            return factory.ReservationDao.All().ToArray();
+        }
     }
 
     public Reservation GetReservation(int id)
     {
-        throw new NotImplementedException();
+        using (DAOFactory factory = new DAOFactory())
+        {
+            Reservation[] reservations = factory.ReservationDao.All().ToArray();
+            return reservations.First(p => p.Id == id);
+        }
     }
 
     public void CreateReservation(Reservation reservation)
     {
-        throw new NotImplementedException();
+        using (DAOFactory factory = new DAOFactory())
+        {
+            Reservation checkedReservation = factory.ReservationDao.All().FirstOrDefault(p => p.Id == reservation.Id);
+            if (checkedReservation != null) 
+                throw new FaultException(new FaultReason("User already exists!!!"), new FaultCode("400"), "");
+            factory.ReservationDao.Add(reservation);
+        }
     }
 
     public void UpdateReservation(Reservation reservation)
     {
-        throw new NotImplementedException();
+        using (DAOFactory factory = new DAOFactory())
+        {
+            Reservation checkedReservation = factory.ReservationDao.All().FirstOrDefault(p => p.Id == reservation.Id);
+            if (checkedReservation == null)
+                throw new FaultException(new FaultReason("Product not found!!!"), new FaultCode("404"), "");
+            factory.ReservationDao.Update(reservation);
+        }
     }
 }
